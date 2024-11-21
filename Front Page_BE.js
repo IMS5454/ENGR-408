ac8483dadf4294343e3e2c688b146a5b791fd40b
IMS5454 <ims5454@psu.edu>
2024-11-20 21:35:42 -0500

    Update Front Page_BE.js

--- Front Page_BE.js
+++ Front Page_BE.js
@@ -1,9 +1,4 @@
-// Function to handle button click and redirect
-function redirectTo(page) {
-    window.location.href = page; // Redirect to the specified page
-}
-
-// Add event listeners to the buttons after the DOM is loaded
+function redirectTo(page) {window.location.href = page;}
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("button1").addEventListener("click", () => redirectTo("test.html"));
     document.getElementById("button2").addEventListener("click", () => redirectTo("test.html"));
